35ceb81965878dd3a21b4f36a266b837cdc7a05c
Evgeniy Malyutin <evxn@users.noreply.github.com>
2021-02-02 16:50:12 +0300

    perf info added to watch script

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 fswatch -t0 "dist/main.js" | \
-    xargs -0 -n1 -I '{}' sh -c 'echo "\n\033[35m[{}]\033[0m"; node --max-old-space-size=100 dist/main.js'
+    xargs -0 -n1 -I '{}' sh -c 'echo "\n\033[35m[{}]\033[0m"; time node --max-old-space-size=4096 dist/main.js'
