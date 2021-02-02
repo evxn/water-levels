2ddbea544c962ce3a2124dd844b9558fb4187371
Evgeniy Malyutin <evxn@users.noreply.github.com>
2021-02-02 05:56:25 +0300

    infrastructure

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 fswatch -t0 "dist/main.js" | \
-    xargs -0 -n1 -I '{}' sh -c 'echo "\n\033[35m[{}]\033[0m"; node dist/main.js'
+    xargs -0 -n1 -I '{}' sh -c 'echo "\n\033[35m[{}]\033[0m"; node --max-old-space-size=100 dist/main.js'
